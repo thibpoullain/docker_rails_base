cdcbcd0c5ffd10eef062339dfaca6b5c31c7ff2c
Thibaut Poullain <thibaut.poullain@ydrazil.fr>
2021-06-04 18:05:50 +0200

    Ruby 3.0.1
    Update readme

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-docker build -t vrizzt/ruby_base_builder:2.7.2 -f ./builder/Dockerfile .
-docker build -t vrizzt/ruby_base_final:2.7.2 -f ./final/Dockerfile .
+docker build -t vrizzt/ruby_base_builder:3.0.1 -f ./builder/Dockerfile .
+docker build -t vrizzt/ruby_base_final:3.0.1 -f ./final/Dockerfile .
 
 echo $DOCKER_HUB_KEY | docker login -u $DOCKER_HUB_USER --password-stdin
-docker push vrizzt/ruby_base_builder:2.7.2
-docker push vrizzt/ruby_base_final:2.7.2
+docker push vrizzt/ruby_base_builder:3.0.1
+docker push vrizzt/ruby_base_final:3.0.1
